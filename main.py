865e96b6172390806257d1a60a1b88acf4243e8e
atrox3d <atrox3d@gmail.com>
2021-05-10 10:32:25 +0200

    reformat main

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    url_for,
+    flash
+)
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import (
+    generate_password_hash,
+    check_password_hash
+)
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from flask_login import (
+    UserMixin,
+    login_user,
+    LoginManager,
+    login_required,
+    current_user,
+    logout_user
+)
 from forms import CreatePostForm
 from flask_gravatar import Gravatar
 
@@ -31,6 +47,8 @@ class BlogPost(db.Model):
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
+
+
 db.create_all()
 
 
