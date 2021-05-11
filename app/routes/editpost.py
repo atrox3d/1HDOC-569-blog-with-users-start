f30819b1971396d418d2f8ba12ea982968573d45
atrox3d <atrox3d@gmail.com>
2021-05-11 07:56:00 +0200

    refactor edit post

---
+++ app/routes/editpost.py
@@ -0,0 +1,28 @@
+from flask import url_for, render_template
+from werkzeug.utils import redirect
+
+import util.logging
+from app.forms import CreatePostForm
+from app.models import BlogPost, db
+
+
+@util.logging.log_decorator()
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        post.author = edit_form.author.data
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for("show_post", post_id=post.id))
+
+    return render_template("make-post.html", form=edit_form)
--- main.py
+++ main.py
@@ -8,7 +8,6 @@
 # import flask_sqlalchemy
 from flask import (
     Flask,
-    render_template,
     redirect,
     url_for,
     # request,
@@ -29,7 +28,6 @@ import logging
 #   project imports
 #
 ########################################################################################################################
-from app.forms import CreatePostForm
 #
 # https://stackoverflow.com/questions/51756650/using-proper-file-structure-with-sqlalchemy-and-how-to-add-data-to-db
 # https://github.com/slezica/bleg/blob/master/data/posts/2014-03-08-avoiding-circular-dependencies-in-flask.md
@@ -119,28 +117,8 @@ contact = app.route("/contact")(contact)
 from app.routes.addpost import add_new_post
 add_new_post = app.route("/new-post")(add_new_post)
 
-
-@app.route("/edit-post/<int:post_id>")
-@util.logging.log_decorator()
-def edit_post(post_id):
-    post = BlogPost.query.get(post_id)
-    edit_form = CreatePostForm(
-        title=post.title,
-        subtitle=post.subtitle,
-        img_url=post.img_url,
-        author=post.author,
-        body=post.body
-    )
-    if edit_form.validate_on_submit():
-        post.title = edit_form.title.data
-        post.subtitle = edit_form.subtitle.data
-        post.img_url = edit_form.img_url.data
-        post.author = edit_form.author.data
-        post.body = edit_form.body.data
-        db.session.commit()
-        return redirect(url_for("show_post", post_id=post.id))
-
-    return render_template("make-post.html", form=edit_form)
+from app.routes.editpost import edit_post
+edit_post = app.route("/edit-post/<int:post_id>")(edit_post)
 
 
 @app.route("/delete/<int:post_id>")
