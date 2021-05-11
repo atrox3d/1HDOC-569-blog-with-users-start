08ea8f5346bbf9e987fada588fde0ec1a505af35
atrox3d <atrox3d@gmail.com>
2021-05-11 08:51:21 +0200

    separate concerns in register.py

--- app/routes/register.py
+++ app/routes/register.py
@@ -4,12 +4,40 @@ from werkzeug.utils import redirect
 
 import logging
 
+import app.forms
 import util.logging
 from app.models import User, db
 
 logger = logging.getLogger(__name__)
-# from main import app, logger
 
+
+# from main import app, logger
+@util.logging.log_decorator()
+def processform(form: app.forms.RegisterForm):
+    logger.info("hash password")
+    email = form.email.data
+    password = form.password.data
+    name = form.name.data
+    return email, password, name
+
+
+@util.logging.log_decorator()
+def createuser(email, password, name):
+    password_hash = generate_password_hash(password, method="pbkdf2:sha256", salt_length=8)
+    logger.info(f"create User({email=}, {password_hash=}, {name=}")
+    user = User(
+        email=email,
+        password=password_hash,
+        name=name
+    )
+    return user
+
+
+@util.logging.log_decorator()
+def adduser(user:User):
+    logger.info("add user")
+    db.session.add(user)
+    db.session.commit()
 
 
 @util.logging.log_decorator()
@@ -17,21 +45,10 @@ def register():
     from app.forms import RegisterForm
     form = RegisterForm()
     if form.validate_on_submit():
-        logger.info("hash password")
-        email = form.email.data
-        password = form.password.data
-        name = form.name.data
-        password_hash = generate_password_hash(password, method="pbkdf2:sha256", salt_length=8)
-        logger.info(f"create User({email=}, {password_hash=}, {name=}")
-        user = User(
-            email=email,
-            password=password_hash,
-            name=name
-        )
+        email, password, name = processform(form)
+        user = createuser(email, password, name)
         try:
-            logger.info("add user")
-            db.session.add(user)
-            db.session.commit()
+            adduser(user)
             url = url_for("get_all_posts")
             logger.info(f"redirect: {url=}")
             return redirect(url)
