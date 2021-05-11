9d2779253b9d03f01d4381a2a56be186578af3c3
atrox3d <atrox3d@gmail.com>
2021-05-11 16:48:20 +0200

    logout

--- app/routes/logout.py
+++ app/routes/logout.py
@@ -1,5 +1,5 @@
 from flask import url_for
-from flask_login import current_user
+from flask_login import current_user, logout_user
 from werkzeug.utils import redirect
 
 import util.logging
@@ -7,4 +7,5 @@ import util.logging
 
 @util.logging.log_decorator()
 def logout():
+    logout_user()
     return redirect(url_for('get_all_posts', loggedin=current_user.is_authenticated))
