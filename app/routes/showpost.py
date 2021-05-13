071ae46154506c2e6f38ba05b0c4ba2b035957fb
atrox3d <atrox3d@gmail.com>
2021-05-13 11:18:34 +0200

    add commentform object as parameter

--- app/routes/showpost.py
+++ app/routes/showpost.py
@@ -2,14 +2,17 @@ from flask import render_template
 from flask_login import current_user
 
 import util.logging
+from app.forms import CommentForm
 from app.models import BlogPost
 
 
 @util.logging.log_decorator()
 def show_post(post_id):
     requested_post = BlogPost.query.get(post_id)
+    commentform = CommentForm()
     return render_template(
         "post.html",
         post=requested_post,
+        form=commentform
         # loggedin=current_user.is_authenticated
     )
